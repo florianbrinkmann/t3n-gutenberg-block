a29afa34e8019f58ced0b9fdd43bde82de84f5ec
Florian Brinkmann <kontakt@florianbrinkmann.com>
2018-07-04 08:35:27 +0200

    Initial commit

---
+++ webpack.config.production.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+	mode: 'production',
+	entry: ['./blocks/post/index.js', './blocks/post/editor.scss'],
+	output: {
+		path: path.resolve(__dirname, 'assets'),
+		filename: 'js/editor.blocks.js',
+	},
+	module: {
+		rules: [
+			/**
+			 * Running Babel on JS files.
+			 */
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@wordpress/default']
+					}
+				}
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'css/editor.blocks.css',
+						}
+					},
+					{
+						loader: 'extract-loader'
+					},
+					{
+						loader: 'css-loader?-url'
+					},
+					{
+						loader: 'postcss-loader'
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			}
+		]
+	}
+};
